4aa7d96cfe2e9ccf1fba33954387ef66449d61e3
Yihan Zhang <shzhangyihan@gmail.com>
2017-06-30 17:26:20 +0900

    better encode added, GRU_v1 added, with test on none trained prespective

--- RNN/GRU_v1.py
+++ RNN/GRU_v1.py
@@ -1,145 +1,133 @@
-from __future__ import print_function, division
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import csv
 import cv2
 
-np.set_printoptions(threshold='nan')
+np.set_printoptions(threshold = 'nan')
 
 # hyperparameters
-num_epochs = 200
-total_series_length = 840
-input_size = 4 + 7
+learning_rate = 0.001
+num_epochs = 100
+total_length = 840
+input_size = 4 + 40
 output_size = input_size
 cell_size = 4 * input_size
-batch_size = 840
-num_batch = 1
 num_layer = 4
-seq_len = 140
+num_batch = 1
+seq_len = 70
 
-# GRU define
-# placeholders
-x_placeholder = tf.placeholder(tf.float32, [num_batch, seq_len, input_size])
-y_placeholder = tf.placeholder(tf.float32, [num_batch, seq_len, output_size])
+root_path = '../Nao_data/resize_4bit_noisy_40/'
+joint_path = '../Nao_data/nao_data_joint.csv'
+
+# GRU graph input
+x_placeholder = tf.placeholder(tf.float32, [None, seq_len, input_size])
+y_placeholder = tf.placeholder(tf.float32, [None, seq_len, output_size])
 Y_ = tf.reshape(y_placeholder, [-1, output_size])
-Hin = tf.placeholder(tf.float32, [num_batch, cell_size * num_layer])
-    
+Hin = tf.placeholder(tf.float32, [None, cell_size * num_layer])
+
 # GRU model
 mcell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.GRUCell(cell_size) for _ in range(num_layer)], state_is_tuple = False)
-Hr, H = tf.nn.dynamic_rnn(mcell, x_placeholder, initial_state = Hin)
+out, state = tf.nn.dynamic_rnn(mcell, x_placeholder, initial_state = Hin)
 
-# output layer
-Hf = tf.reshape(Hr, [-1, cell_size])
-Y = tf.contrib.layers.linear(Hf, output_size)
+# output
+Y = tf.contrib.layers.linear(tf.reshape(out, [-1, cell_size]), output_size)
 
-# loss and training step
+# loss and optimizer
 loss = tf.contrib.losses.mean_squared_error(Y, Y_)
-train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
-def readData(root_path, is_self):
-    print("root = ", root_path)
-    array = []
+def readData(encode_num, is_self):
+    file_path = root_path + 'encode_' + str(encode_num) + '.txt'
+    print 'file = ', file_path
     if is_self:
-        print("TRUE")
-        file_path = root_path + "/nao_data_joint.csv"
-        f = open(file_path, 'rb')
+        array = []
+        f = open(joint_path, 'rb')
         dataReader = csv.reader(f)
-        iterator = 1
         for row in dataReader:
-            img_path = root_path + "/camImage" + str(iterator) + ".png"
-            img = cv2.imread(img_path, 0)
-            data_set = img.flatten()
-            data_set = map(lambda x: x/255, data_set)
-            data_set = np.append(data_set, np.asarray(row))
-            print(data_set.shape)
-            array.append(data_set)
-            iterator += 1
+            array.append(row)
+        data = map(lambda x:[float(v) for v in x], array)
+        data = np.asarray(data)
     else:
-        print("FALSE")
-        iterator = 1
-        img_path = root_path + "/camImage" + str(iterator) + ".png"
-        img = cv2.imread(img_path, 0)
-        while not img is None:
-            data_set = img.flatten()
-            data_set = map(lambda x: x/255, data_set)
-            data_set = np.append(data_set, np.zeros(4))
-            print(data_set.shape)
-            array.append(data_set)
-            iterator += 1
-            img_path = root_path + "/camImage" + str(iterator) + ".png"
-            img = cv2.imread(img_path, 0)
-        
-    data = map(lambda x:[float(v) for v in x], array)
-    datax = np.asarray(data)
-    datay = np.roll(data, 1, axis = 0)
+        data = np.zeros([total_length, 4])
     
-    actual = datay
-    datax = datax.reshape((num_batch, -1, input_size))
-    datay = datay.reshape((num_batch, -1, input_size))
-
-    print("x shape: ", datax.shape)
-    print("x = ", np.asarray(datax))
-    print("y = ", np.asarray(datay))
+    encode_data = np.loadtxt(file_path)
+    data = np.concatenate((data, encode_data), axis = 1)
+    data_x = data
+    data_x = data_x.reshape((num_batch, -1, input_size))
+    data_y = np.roll(data, 1, axis = 0)
+    actual = data_y
+    data_y = data_y.reshape((num_batch, -1, input_size))
     
-    return(datax, datay, actual)
+    return(data_x, data_y, actual)
 
-def plot(loss_list, prediction_series, actual_series):
+def test(data_set):
+    x, y, actual = readData(data_set, False)
+    inH = np.zeros([num_batch, cell_size * num_layer])
+    start_index = 0
+    pred_series = []
+    while start_index < total_length:
+        print(start_index)
+        X = x[:, start_index: start_index + seq_len, :]
+        Y_ = y[:, start_index: start_index + seq_len, :]
+        dic = {x_placeholder: X, y_placeholder: Y_, Hin: inH}
+        l, outH, pred = sess.run([loss, state, Y], feed_dict = dic)
+        start_index += seq_len
+        print(l)
+        for i in range(seq_len):
+            pred_series.append(pred[i, :])
+    plot(None, pred_series, actual, True)
+
+def plot(loss_list, prediction_series, actual_series, need_press):
+    print 'p'
     p = plt.subplot(2, 3, 1)
     plt.cla()
-    plt.plot(loss_list)
+    if loss_list is not None:
+        plt.plot(loss_list)
     p.set_title("Loss function")
     
-    iterator = 0
     for iterator in range(4):
         p = plt.subplot(2, 3, iterator + 3)
-        dimension = 1200 + iterator
         plt.cla()
-        plt.axis([0, total_series_length, -0.5, 1.5])
-        p.plot(np.asarray(prediction_series)[:, dimension], color = 'r')
-        p.plot(np.asarray(actual_series)[:, dimension], color = 'b')
-        title = "Input dimension " + str(iterator)
+        plt.axis([0, total_length, 0, 1])
+        p.plot(np.asarray(prediction_series)[:, iterator], color = 'r')
+        p.plot(np.asarray(actual_series)[:, iterator], color = 'b')
+        title = "Joint info #" + str(iterator + 1)
         p.set_title(title)
     
     plt.draw()
-    plt.pause(0.00001)
+    if need_press:
+        plt.waitforbuttonpress()
+    else:
+        plt.pause(0.00001)
 
-def train_neural_network():
-    x, y, actual = readData("../test_data/Degree_0/Data_1", True)
-    loss_list = []
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(num_epochs):
-            inH = np.zeros([num_batch, cell_size * num_layer])
-            epoch_loss = 0
-            start_index = 0
-            pred_series = []
-            while start_index < total_series_length:
-                X = x[:, start_index: start_index+seq_len, :]
-                print(X.shape)
-                Y_ = y[:, start_index: start_index+seq_len, :]
-                dic = {x_placeholder: X, y_placeholder:Y_, Hin:inH}
-                # training start
-                _, loss_, outH, pred = sess.run([train_step, loss, H, Y], feed_dict = dic)
-                inH = outH
-                epoch_loss += loss_
-                start_index += seq_len
-                if epoch % 5 == 0:
-                    for i in range(seq_len):
-                        pred_series.append(pred[i,:])
-            # end of epoch
-            loss_list.append(epoch_loss)
-            print("Epoch ", epoch, "loss: ", epoch_loss)
-            # plot
+x, y, actual = readData(8, True)
+loss_list = []
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for epoch in range(num_epochs):
+        inH = np.zeros([num_batch, cell_size * num_layer])
+        epoch_loss = 0
+        start_index = 0
+        pred_series = []
+        while start_index < total_length:
+            X = x[:, start_index: start_index + seq_len, :]
+            Y_ = y[:, start_index: start_index + seq_len, :]
+            dic = {x_placeholder: X, y_placeholder: Y_, Hin: inH}
+            _, l, outH, pred = sess.run([optimizer, loss, state, Y], feed_dict = dic)
+            inH = outH
+            epoch_loss += l
+            start_index += seq_len
+            # prepare for plot
             if epoch % 5 == 0:
-                print("pred", np.asarray(pred_series).shape)
-                print("actu", np.asarray(actual).shape)
-                plot(loss_list, pred_series, actual)
-
-plt.ioff()
-plt.show()
-train_neural_network()
-#readData("../test_data/Degree_0/Data_1", True)
-#readData("../test_data/Degree_90/Data_1", False)
+                for i in range(seq_len):
+                    pred_series.append(pred[i, :])
+        
+        loss_list.append(epoch_loss)
+        print("Epoch ", epoch, " loss: ", epoch_loss)
+        if epoch % 5 == 0:
+            plot(loss_list, pred_series, actual, False)
+    test(0)
 

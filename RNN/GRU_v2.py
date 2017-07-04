efb8587b28ba0f6da7df9ae3aef4d2c038a96f34
shzhangyihan <shzhangyihan@gmail.com>
2017-07-04 18:57:47 +0900

    GRU_v2 modified, plot not changed

--- RNN/GRU_v2.py
+++ RNN/GRU_v2.py
@@ -15,7 +15,8 @@ input_size = 4 + 40
 output_size = input_size
 cell_size = 4 * input_size
 num_layer = 4
-num_batch = 1
+num_batch = 2
+batch_size = 1
 seq_len = 70
 
 root_path = '../Nao_data/resize_4bit_noisy_40/'
@@ -38,38 +39,53 @@ Y = tf.contrib.layers.linear(tf.reshape(out, [-1, cell_size]), output_size)
 loss = tf.contrib.losses.mean_squared_error(Y, Y_)
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
-def readData(encode_num, is_self):
-    file_path = root_path + 'encode_' + str(encode_num) + '.txt'
-    print 'file = ', file_path
-    if is_self:
-        array = []
-        f = open(joint_path, 'rb')
-        dataReader = csv.reader(f)
-        for row in dataReader:
-            array.append(row)
-        data = map(lambda x:[float(v) for v in x], array)
-        data = np.asarray(data)
-    else:
-        data = np.zeros([total_length, 4])
+def readData():
+    x = np.zeros([num_batch, batch_size, total_length, input_size])
+    y = np.zeros([num_batch, batch_size, total_length, input_size])
+    actual = np.zeros([num_batch, total_length, input_size])
+    for batch in range(num_batch):
+        b = (batch - 1) % 9
+        file_path = root_path + 'encode_' + str(b) + '.txt'
+        # is self, load joint
+        if b == 8:
+            array = []
+            f = open(joint_path, 'rb')
+            dataReader = csv.reader(f)
+            for row in dataReader:
+                array.append(row)
+            data = map(lambda x: [float(v) for v in x], array)
+            data = np.asarray(data)
+        else:
+            data = np.zeros([total_length, 4])
+        
+        encode_data = np.loadtxt(file_path)
+        data = np.concatenate((data, encode_data), axis = 1)
+        data_x = data.reshape((batch_size, total_length, input_size))
+        data_y = np.roll(data, 1, axis = 0)
+        act = data_y
+        data_y = data_y.reshape((batch_size, total_length, input_size))
+        x[batch] = data_x
+        y[batch] = data_y
+        actual[batch] = act
     
-    encode_data = np.loadtxt(file_path)
-    data = np.concatenate((data, encode_data), axis = 1)
-    data_x = data
-    data_x = data_x.reshape((num_batch, -1, input_size))
-    data_y = np.roll(data, 1, axis = 0)
-    actual = data_y
-    data_y = data_y.reshape((num_batch, -1, input_size))
+    print x.shape
+    print y.shape
+    print actual.shape
     
-    return(data_x, data_y, actual)
+    return(x, y, actual)
 
 def updataData(x, pred):
+    y = x
     pred = np.asarray(pred)
-    pred = pred.reshape((num_batch, -1, input_size))
-    print x.shape
-    print pred.shape
+    pred_x = np.roll(pred, -1, axis = 0)
+    pred_x = pred_x.reshape((batch_size, total_length, input_size))
+    pred_y = pred.reshape((batch_size, total_length, input_size))
+#    print x.shape
+#    print pred.shape
     for i in range(4):
-        x[0, :, i] = pred[0, :, i]
-    return(x, x)
+        x[0, :, i] = pred_x[0, :, i]
+        y[0, :, i] = pred_y[0, :, i]
+    return(x, y)
 
 def test(data_set):
     x, y, actual = readData(data_set, False)
@@ -114,76 +130,32 @@ def plot(loss_list, prediction_series, actual_series, need_press):
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     
-    x, y, actual = readData(8, True)
+    x_b, y_b, actual_b = readData()
     loss_list = []
     for epoch in range(num_epochs):
-        inH = np.zeros([num_batch, cell_size * num_layer])
         epoch_loss = 0
-        start_index = 0
-        pred_series = []
-        while start_index < total_length:
-            X = x[:, start_index: start_index + seq_len, :]
-            Y_ = y[:, start_index: start_index + seq_len, :]
-            dic = {x_placeholder: X, y_placeholder: Y_, Hin: inH}
-            _, l, outH, pred = sess.run([optimizer, loss, state, Y], feed_dict = dic)
-            inH = outH
-            epoch_loss += l
-            start_index += seq_len
-            # prepare for plot
-            if epoch % 5 == 0:
+        for batch in range(num_batch):
+            x = x_b[batch]
+            y = y_b[batch]
+            actual = actual_b[batch]
+            inH = np.zeros([num_batch, cell_size * num_layer])
+            start_index = 0
+            pred_series = []
+            while start_index < total_length:
+                X = x[:, start_index: start_index + seq_len, :]
+                Y_ = y[:, start_indexL start_index + seq_len, :]
+                dic = {x_placeholder: X, y_placeholder: Y_, Hin: inH}
+                _, l, outH, pred = sess.run([optimizer, loss, state, Y], feed_dict = dic)
+                inH = outH
+                epoch_loss += l
+                start_index += seq_len
                 for i in range(seq_len):
                     pred_series.append(pred[i, :])
-        
+            # is not self, update joint
+            if batch != 0:
+                x_b[batch], y_b[batch] = updateData(x, pred_series)
         loss_list.append(epoch_loss)
         print("Epoch ", epoch, " loss: ", epoch_loss)
         if epoch % 5 == 0:
-            plot(loss_list, pred_series, actual, False)
-    
-    x, y, actual = readData(0, False)
-    for epoch in range(num_epochs_other):
-        print x
-        inH = np.zeros([num_batch, cell_size * num_layer])
-        epoch_loss = 0
-        start_index = 0
-        pred_series = []
-        while start_index < total_length:
-            X = x[:, start_index: start_index + seq_len, :]
-            Y_ = y[:, start_index: start_index + seq_len, :]
-            dic = {x_placeholder: X, y_placeholder: Y_, Hin: inH}
-            _, l, outH, pred = sess.run([optimizer, loss, state, Y], feed_dict = dic)
-            inH = outH
-            epoch_loss += l
-            start_index += seq_len
-            for i in range(seq_len):
-                pred_series.append(pred[i, :])
-                
-        loss_list.append(epoch_loss)
-        print("Epoch ", epoch, " loss: ", epoch_loss)
-        if num_epochs_other % 5 == 0:
-            plot(loss_list, pred_series, actual, False)
-        if num_epochs_other % 99 == 0:
-            plot(loss_list, pred_series, actual, True)
-        
-        x, y = updataData(x, pred_series)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            plot(loss_list)
 
